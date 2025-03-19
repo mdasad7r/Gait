981c75c3fa7709dea448a2a060f5b5a134ece22c
mdasad7r <asadmohd9411@gmail.com>
2025-03-20 03:09:59 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+import torch
+
+def calculate_accuracy(model, dataloader, device):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return 100 * correct / total
