5c210aac152d7877f51a36f2115869f43f13861b
mdasad7r <asadmohd9411@gmail.com>
2025-03-20 03:11:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+TRAIN_DIR = "/content/casia-b/train"
+TEST_DIR = "/content/casia-b/test"
+BATCH_SIZE = 32
+LEARNING_RATE = 0.001
+NUM_EPOCHS = 10
+SAVE_MODEL_PATH = "/content/drive/MyDrive/gait_recognition_model.pth"
