546898ad62e8f8a705e243b305e09fc84b3b2c35
mdasad7r <asadmohd9411@gmail.com>
2025-04-08 23:09:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ TRAIN_DIR = "/content/casia-b/train/output"
 TEST_DIR = "/content/casia-b/test/output"
 BATCH_SIZE = 32
 LEARNING_RATE = 0.001
-NUM_EPOCHS = 10
+NUM_EPOCHS = 50
+SEQUENCE_LEN = 50  # Centralized sequence length
 SAVE_MODEL_PATH = "/content/drive/MyDrive/gait_recognition_model.pth"
