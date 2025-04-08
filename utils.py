b06ef9637de91064cb5ac6cdeb3af1da390fe392
mdasad7r <asadmohd9411@gmail.com>
2025-04-08 23:20:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,3 +12,16 @@ def calculate_accuracy(model, dataloader, device):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
     return 100 * correct / total
+
+def calculate_top5_accuracy(model, dataloader, device):
+    model.eval()
+    top5_correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, top5_preds = outputs.topk(5, dim=1)
+            total += labels.size(0)
+            top5_correct += (top5_preds == labels.view(-1, 1)).sum().item()
+    return 100 * top5_correct / total
